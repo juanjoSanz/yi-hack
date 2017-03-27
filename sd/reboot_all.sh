b12bed329b900d6543f6a156520b20c9ad8b927d
juanjoSanz <juanjo.sanzmartin@gmail.com>
2017-03-27 16:59:01 +0200

    Update reboot_all.sh

--- sd/reboot_all.sh
+++ sd/reboot_all.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
+#set -x
 
 get_config() {
     key=$1
-    grep "^$1="./test/yi-hack.cfg  | cut -d"=" -f2
+    grep "^$1=" ./test/yi-hack.cfg  | cut -d'=' -f2
 }
 
+pass=$(get_config ROOT_PASSWORD)
+IPs="192.168.2.20 192.168.2.21 192.168.2.22 192.168.2.23 192.168.2.24"
 
 
-for srv in $(echo "192.168.2.20 192.168.2.21 192.168.2.22 192.168.2.23 192.168.2.24")
+for srv in $(echo $IPs)
 do
    echo "Restarting ... $srv"
-   ./reboot.expect $srv 23 root $(get_config ROOT_PASSWORD)
-
-
+   ./reboot.expect $srv 23 root $pass
 
 done
